5183c4eb5df1f4cdec25a7b27857c6b914d21eda
CESCO <cescovalledev@gmail.com>
2024-02-06 20:29:15 -0600

    Beggining web portfolio

---
+++ src/WebPortfolioApp.tsx
@@ -0,0 +1,24 @@
+export const WebPortfolioApp = () => {
+  return (
+    <>
+      <div className="bg-background text-background-foreground">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="bg-primary text-primary-foreground">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="bg-secondary text-secondary-foreground">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="bg-muted text-muted-foreground">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="bg-accent text-accent-foreground">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="bg-destructive text-destructive-foreground">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+};
