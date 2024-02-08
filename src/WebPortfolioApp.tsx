078f44d1153c1dc1310950ee1f29be9d7fa72da4
CESCO <cescovalledev@gmail.com>
2024-02-07 23:26:13 -0600

    added buttons drawer and navmenu from shadcn

--- src/WebPortfolioApp.tsx
+++ src/WebPortfolioApp.tsx
@@ -1,6 +1,10 @@
+import { DrawerMenu, NavMenu } from "./components/NavBar";
+
 export const WebPortfolioApp = () => {
   return (
     <>
+      <DrawerMenu />
+      <NavMenu />
       <div className="bg-background text-background-foreground">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
---
+++ src/components/NavBar/NavMenu.tsx
@@ -0,0 +1,41 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  //   NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  //   NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+export const NavMenu = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem className="p-1">
+          <NavigationMenuLink href="#home">Home</NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="p-1">
+          <NavigationMenuLink href="#about">About</NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="p-1">
+          <NavigationMenuLink href="#services">Services</NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="p-1">
+          <NavigationMenuLink href="#skills">Skills</NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="p-1">
+          <NavigationMenuLink href="#projects">Projects</NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="p-1">
+          <NavigationMenuLink href="#contacts">Contact Me</NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="p-1">
+          <NavigationMenuTrigger>English</NavigationMenuTrigger>
+          <NavigationMenuContent>Español</NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
