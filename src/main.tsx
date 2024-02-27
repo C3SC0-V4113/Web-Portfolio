c9816ccab8f551bccbfb35748b2513ad4d513af8
CESCO <cescovalledev@gmail.com>
2024-02-27 17:00:40 -0600

    themes added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { WebPortfolioApp } from "./WebPortfolioApp";
 import "./index.css";
+import { ThemeProvider } from "./components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WebPortfolioApp />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <WebPortfolioApp />
+    </ThemeProvider>
   </React.StrictMode>
 );
